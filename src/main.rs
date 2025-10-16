10a0cf6579bbfa86cd29c858b245d47c9bde0263
Paulo Cunha <pc@pmrcunha.com>
2025-10-16 22:25:55 +0200

    Add some error messages

--- src/main.rs
+++ src/main.rs
@@ -32,26 +32,37 @@ async fn main() -> Result<(), Error> {
                 .possible_values(&["sats", "eur"])
                 .help("Select if your input is sats or euros"),
         )
-        .arg(
-            Arg::with_name("value")
-            .help("The value to convert")
-        )
+        .arg(Arg::with_name("value").help("The value to convert"))
         .get_matches();
-    
+
     let currency = matches.value_of("input currency").unwrap_or("sats");
-    let input = matches.value_of("value").unwrap();
-    let input_nr = input.parse::<f32>().unwrap();
+    let input = matches
+        .value_of("value")
+        .expect("You must provide a value to convert!");
+    let input_nr = input
+        .parse::<f32>()
+        .expect("Provided value is not a number.");
 
     let request_url = format!("https://api.kraken.com/0/public/Ticker?pair=BTCEUR");
     let response = reqwest::get(&request_url).await?;
 
     let prices: Price = response.json().await?;
-    let btceur_price = prices.result.XXBTZEUR.c[0].parse::<f32>().unwrap();
+    let btceur_price = prices.result.XXBTZEUR.c[0]
+        .parse::<f32>()
+        .expect("Failed to parse price received from Kraken.");
 
     let result = match currency {
-        "sats" => format!("{} sats are {} euros.", input_nr, input_nr * btceur_price / 100000000f32),
-        "eur" => format!("{} euros are {} sats.", input_nr, input_nr / btceur_price * 100000000f32),
-        _ => panic!("Invalid code flow - unsuported currency")
+        "sats" => format!(
+            "{} sats are {} euros.",
+            input_nr,
+            input_nr * btceur_price / 100000000f32
+        ),
+        "eur" => format!(
+            "{} euros are {} sats.",
+            input_nr,
+            input_nr / btceur_price * 100000000f32
+        ),
+        _ => panic!("Invalid code flow - unsuported currency"),
     };
 
     println!("{}", result);
